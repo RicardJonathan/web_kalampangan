646add53dfca7e7e623b35c4f7ea1150e9f3817b
RicardJonathan <ricardjonathan36@gamil.com>
2025-04-28 09:24:07 +0700

    push db

---
+++ DB/db_kelurahan_kalampangan.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2025 at 04:20 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kelurahan_kalampangan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `nip` varchar(20) DEFAULT NULL,
+  `jabatan` varchar(100) DEFAULT NULL,
+  `pangkat` varchar(50) DEFAULT NULL,
+  `golongan` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `nama`, `nip`, `jabatan`, `pangkat`, `golongan`, `username`, `password`) VALUES
+(1, 'Ricard', '198012122020011001', 'admin kepala', 'Pembina Utama Muda', 'IV/c', 'admin', 'admin123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lurah`
+--
+
+CREATE TABLE `lurah` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `nip` varchar(20) DEFAULT NULL,
+  `pangkat` varchar(50) DEFAULT NULL,
+  `golongan` varchar(50) DEFAULT NULL,
+  `ttd` varchar(255) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengajuan_surat`
+--
+
+CREATE TABLE `pengajuan_surat` (
+  `id` int(11) NOT NULL,
+  `jenis_surat` enum('Surat Keterangan Tidak Mampu','Surat Keterangan Kematian','Surat Keterangan Kelahiran','Surat Keterangan Pindah','Surat Keterangan Belum Menikah','Surat Keterangan Untuk Menikah','Pengajuan PBB Baru','Surat Keterangan Ahli Waris','Surat Keterangan Berkelakuan Baik','Surat Keterangan Domisili') NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nama_pengaju` varchar(255) NOT NULL,
+  `email_pengaju` varchar(100) NOT NULL,
+  `no_telepon` varchar(15) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `tgl_pengajuan` date NOT NULL,
+  `status` enum('Menunggu','Diterima','Ditolak') NOT NULL,
+  `keterangan` text DEFAULT NULL,
+  `foto_ktp` varchar(255) DEFAULT NULL,
+  `foto_kk` varchar(255) DEFAULT NULL,
+  `foto_formulir` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `nik` varchar(20) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `no_telepon` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `nik`, `username`, `no_telepon`, `email`, `password`) VALUES
+(1, 'user1', '12345678', 'user11', '081234567890', 'user1@example.com', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lurah`
+--
+ALTER TABLE `lurah`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `pengajuan_surat`
+--
+ALTER TABLE `pengajuan_surat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `lurah`
+--
+ALTER TABLE `lurah`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengajuan_surat`
+--
+ALTER TABLE `pengajuan_surat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pengajuan_surat`
+--
+ALTER TABLE `pengajuan_surat`
+  ADD CONSTRAINT `pengajuan_surat_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
